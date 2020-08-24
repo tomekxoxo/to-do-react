5d14d9f7de9c6b99b85d96f8e5542b5e7428b717
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-08-24 14:30:17 +0200

    Updates

---
+++ precache-manifest.a75d7e35d0005e7c741be8e832513b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7660c4c1237acc1fae58aa82be23a9cd",
+    "url": "/to-do-react/index.html"
+  },
+  {
+    "revision": "738697a2f27e186d48fb",
+    "url": "/to-do-react/static/css/main.68d2be30.chunk.css"
+  },
+  {
+    "revision": "13ec543c7a1c74bf8afe",
+    "url": "/to-do-react/static/js/2.e0c87ce8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-react/static/js/2.e0c87ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738697a2f27e186d48fb",
+    "url": "/to-do-react/static/js/main.93fee81a.chunk.js"
+  },
+  {
+    "revision": "a4a7596da2d0dccecb31",
+    "url": "/to-do-react/static/js/runtime-main.3a312704.js"
+  }
+]);
